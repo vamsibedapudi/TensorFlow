be5d0d328bd8146e2745eaa65d930d5c0b6c8566
VamsiBedapudi <vmsbdpd@gmail.com>
2017-10-21 15:54:27 +0530

    using timdist

--- test.py
+++ test.py
@@ -1,23 +1,36 @@
-from keras.layers import Input, Dense
-from keras.models import Model
-from keras.utils import plot_model
-
+import keras
 
 # This returns a tensor
-inputs = Input(shape=(784,))
+input_list = []
 
-# a layer instance is callable on a tensor, and returns a tensor
-x = Dense(64, activation='relu')(inputs)
-x = Dense(64, activation='relu')(x)
-predictions = Dense(10, activation='softmax')(x)
+for i in range(0,15):
+    input_list.append(keras.layers.Input(shape=(23,)))
+
+hidden_layer1_list = []
+
+for i in range(2,13):
+    hidden_layer1_list.append(keras.layers.Dense(512,activation='relu')(keras.layers.concatenate( [input_list[i-2], input_list[i-1], input_list[i], input_list[i+1], input_list[i+2]])))
+
+hidden_layer2_list = []
+hidden_layer2_list.append(keras.layers.Dense(512,activation='relu')(keras.layers.concatenate([hidden_layer1_list[0],hidden_layer1_list[2],hidden_layer1_list[4]])))
+hidden_layer2_list.append(keras.layers.Dense(512,activation='relu')(keras.layers.concatenate([hidden_layer1_list[3],hidden_layer1_list[5],hidden_layer1_list[7]])))
+hidden_layer2_list.append(keras.layers.Dense(512,activation='relu')(keras.layers.concatenate([hidden_layer1_list[6],hidden_layer1_list[8],hidden_layer1_list[10]])))
+
+
+hidden_layer3 = keras.layers.Dense(512,activation='relu')(keras.layers.concatenate([hidden_layer2_list[0],hidden_layer2_list[1],hidden_layer2_list[2]]))
+hidden_layer4 = keras.layers.Dense(512, activation='relu')(hidden_layer3)
+hidden_layer5 = keras.layers.Dense(512, activation='softmax')(hidden_layer4)
+
+# predictions = keras.layers.Dense(1, activation='softmax')(x)
 
 # This creates a model that includes
 # the Input layer and three Dense layers
-model = Model(inputs=inputs, outputs=predictions)
+model = keras.models.Model(inputs=input_list, outputs=hidden_layer5)
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 # model.fit(data, labels)  # starts training
-
-plot_model(model, to_file='model.png')
+print(model.summary())
+keras.utils.plot_model(model, to_file='model.png')
 print("done")
+
